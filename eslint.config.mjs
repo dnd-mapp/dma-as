2500aad6f521f18ccac9985667e01f616a0eaa1a
NoNamer777 <oscar.wellner@gmail.com>
2025-04-21 22:10:26 +0200

    Refactor eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,38 @@
-// @ts-check
 import eslint from '@eslint/js';
+import pluginBan from 'eslint-plugin-ban';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  {
+export default tseslint.config({
+    files: ['**/*.ts'],
     ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
+    plugins: {
+        ban: pluginBan,
+    },
+    extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.recommended,
+        ...tseslint.configs.stylistic,
+        eslintPluginPrettierRecommended,
+    ],
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+        globals: {
+            ...globals.node,
+            ...globals.jest,
+        },
+        sourceType: 'commonjs',
+        parserOptions: {
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+        },
     },
-  },
-  {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+        '@typescript-eslint/no-floating-promises': 'off',
+        'ban/ban': [
+            2,
+            { name: 'fdescribe', message: 'Focussing test suites is not allowed' },
+            { name: 'fit', message: 'Focussing a single unit test is not allowed' },
+        ],
     },
-  },
-);
+});
