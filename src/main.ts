a8f7663a10ca407088360d8b92f2be5a7f4bf721
NoNamer777 <oscar.wellner@gmail.com>
2025-04-21 23:54:36 +0200

    Initialize app with https and override logger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,51 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { AppModule } from './app/app.module';
+import { readFile } from 'fs/promises';
+import { AppModule, SSLConfig } from './app';
+import { DmaLogger } from './app/logging';
 
 async function bootstrap() {
-    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+    const appContext = await NestFactory.createApplicationContext(AppModule, {
+        logger: false,
+    });
+    const configService = appContext.get(ConfigService);
 
-    const port = process.env['PORT'] ?? 3000;
-    const host = process.env['HOST'] ?? '0.0.0.0';
+    const ssl = configService.get<SSLConfig | null>('ssl');
+
+    if (ssl) {
+        ssl.cert = await readFile(ssl.cert, { encoding: 'utf8' });
+        ssl.key = await readFile(ssl.key, { encoding: 'utf8' });
+    }
+    const fastifyAdapter = new FastifyAdapter(
+        !ssl
+            ? {}
+            : {
+                  http2: true,
+                  https: {
+                      cert: ssl.cert,
+                      key: ssl.key,
+                  },
+              }
+    );
+
+    const host = configService.get<string>('host');
+    const port = configService.get<number>('port');
+
+    await appContext.close();
+
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, fastifyAdapter, {
+        bufferLogs: true,
+    });
+    const logger = app.get(DmaLogger);
+
+    app.useLogger(logger);
 
     await app.listen(port, host);
+    logger.log(
+        `Server started on address: "http${ssl ? 's' : ''}://${host === '0.0.0.0' ? 'localhost' : host}:${port}"`,
+        'NestApplication'
+    );
 }
 
 (async () => {
