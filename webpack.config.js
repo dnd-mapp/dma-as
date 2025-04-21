bac16f7472d012351756bb44c7f4e329324ea2ae
NoNamer777 <oscar.wellner@gmail.com>
2025-04-22 00:11:31 +0200

    Upate webpack config to enable dev mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,24 @@ const { resolve } = require('path');
 
 /** @type {import('webpack').Configuration} */
 const webpackConfig = {
+    entry: resolve(__dirname, 'src', 'main.ts'),
+    externalsPresets: {
+        node: true,
+    },
     mode: process.env['NODE_ENV'] === 'production' ? 'production' : 'development',
-    entry: 'src/main.ts',
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    optimization: {
+        nodeEnv: false,
+    },
     output: {
         clean: true,
         path: resolve(__dirname, 'dist', 'dma-as'),
         filename: 'main.js',
     },
+    watch: process.env['NODE_ENV'] === 'development',
 };
 
 module.exports = webpackConfig;
