c61900f74f7f69f062f87b7238686c76ed1324f2
NoNamer777 <oscar.wellner@gmail.com>
2025-04-22 16:09:58 +0200

    Generate package.json in output bundle folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const { resolve } = require('path');
 const nodeExternals = require('webpack-node-externals');
+const GeneratePackageJsonPlugin = require('generate-package-json-webpack-plugin');
+const packageJson = require('./package.json');
 
 /** @type {import('webpack').Configuration} */
 const webpackConfig = {
@@ -21,6 +23,7 @@ const webpackConfig = {
         path: resolve(__dirname, 'dist', 'dma-as'),
         filename: 'main.js',
     },
+    plugins: [new GeneratePackageJsonPlugin(packageJson)],
     watch: process.env['NODE_ENV'] === 'development',
 };
 
