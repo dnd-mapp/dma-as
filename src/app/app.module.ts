da93f76cabcbdd06988b663397c1f7b4fc8fd975
NoNamer777 <oscar.wellner@gmail.com>
2025-04-24 00:58:34 +0200

    Implement simple rate limiting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { configOptions } from './config';
+import { configOptions, provideThrottlerGuard, throttlerOptions } from './config';
 import { DatabaseModule } from './database';
 import { LoggingModule } from './logging';
 import { UsersModule } from './users';
 
 @Module({
-    imports: [ConfigModule.forRoot(configOptions), DatabaseModule, UsersModule, LoggingModule],
+    imports: [
+        ConfigModule.forRoot(configOptions),
+        ThrottlerModule.forRoot(throttlerOptions),
+        DatabaseModule,
+        UsersModule,
+        LoggingModule,
+    ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [AppService, provideThrottlerGuard()],
 })
 export class AppModule {}
