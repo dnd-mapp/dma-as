bd64b588d1d36541447d20beaefbad8497427a90
NoNamer777 <oscar.wellner@gmail.com>
2025-04-23 23:54:22 +0200

    Add database module and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configOptions } from './config';
+import { DatabaseModule } from './database';
 import { LoggingModule } from './logging';
+import { UsersModule } from './users';
 
 @Module({
-    imports: [ConfigModule.forRoot(configOptions)],
+    imports: [ConfigModule.forRoot(configOptions), DatabaseModule, UsersModule, LoggingModule],
     controllers: [AppController],
     providers: [AppService],
 })
