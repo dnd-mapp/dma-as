0bab3e17f8e7e9357b8d496040244ed01183987c
NoNamer777 <oscar.wellner@gmail.com>
2025-04-29 13:42:51 +0200

    Rename User Prisma model

--- src/app/users/users.repository.ts
+++ src/app/users/users.repository.ts
@@ -7,21 +7,21 @@ import { CreateUserData, UpdateUserData, User } from '../shared';
 export class UsersRepository {
     constructor(private readonly databaseService: DatabaseService) {}
 
-    public findAll = async () => plainToInstance(User, await this.databaseService.users.findMany());
+    public findAll = async () => plainToInstance(User, await this.databaseService.user.findMany());
 
     public findOneById = async (userId: string) =>
-        plainToInstance(User, await this.databaseService.users.findFirst({ where: { id: userId } }));
+        plainToInstance(User, await this.databaseService.user.findFirst({ where: { id: userId } }));
 
     public findOneByUsername = async (username: string) =>
-        plainToInstance(User, await this.databaseService.users.findFirst({ where: { username: username } }));
+        plainToInstance(User, await this.databaseService.user.findFirst({ where: { username: username } }));
 
     public update = async (data: UpdateUserData) =>
-        plainToInstance(User, await this.databaseService.users.update({ where: { id: data.id }, data: data }));
+        plainToInstance(User, await this.databaseService.user.update({ where: { id: data.id }, data: data }));
 
     public create = async (data: CreateUserData) =>
-        plainToInstance(User, await this.databaseService.users.create({ data: data }));
+        plainToInstance(User, await this.databaseService.user.create({ data: data }));
 
     public async removeById(userId: string) {
-        await this.databaseService.users.delete({ where: { id: userId } });
+        await this.databaseService.user.delete({ where: { id: userId } });
     }
 }
