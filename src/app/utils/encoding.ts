a499442116c25e522870320f0a5b24e482a26644
NoNamer777 <oscar.wellner@gmail.com>
2025-04-29 13:45:30 +0200

    Add function to create a SHA256 hash from a string

--- src/app/utils/encoding.ts
+++ src/app/utils/encoding.ts
@@ -1,7 +1,10 @@
 import { genSalt, hash } from 'bcryptjs';
+import { createHash } from 'crypto';
 
 export const valueToBase64 = (plain: string) => Buffer.from(plain).toString('base64url');
 
 export const base64ToValue = (encoded: string) => Buffer.from(encoded, 'base64url').toString('utf8');
+
+export const valueToSHA256 = (value: string) => createHash('sha256').update(value).digest();
 
 export const hashPassword = async (password: string) => await hash(password, await genSalt(16));
