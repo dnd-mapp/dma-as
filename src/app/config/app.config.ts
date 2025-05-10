e219f7cb243c4d77134d39cd8835c341601c667a
NoNamer777 <oscar.wellner@gmail.com>
2025-05-10 08:46:57 +0300

    Add encryption key environment variable

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -3,12 +3,13 @@ import { Expose, plainToInstance, Type } from 'class-transformer';
 import { IsArray, IsOptional, IsPort, IsString, MinLength, validate } from 'class-validator';
 import { parseNumber } from '../utils';
 
+const ENV_NAME_ALLOWED_ORIGINS = 'ALLOWED_ORIGINS' as const;
+const ENV_NAME_COOKIE_SIGNING_SECRET = 'COOKIE_SIGNING_SECRET' as const;
+const ENV_NAME_ENCRYPTION_KEY = 'ENCRYPTION_KEY' as const;
 const ENV_NAME_HOST = 'HOST' as const;
 const ENV_NAME_PORT = 'PORT' as const;
 const ENV_NAME_SSL_CERT_PATH = 'SSL_CERT_PATH' as const;
 const ENV_NAME_SSL_KEY_PATH = 'SSL_KEY_PATH' as const;
-const ENV_NAME_COOKIE_SIGNING_SECRET = 'COOKIE_SIGNING_SECRET' as const;
-const ENV_NAME_ALLOWED_ORIGINS = 'ALLOWED_ORIGINS' as const;
 
 export class SSLConfig {
     cert: string;
@@ -16,11 +17,12 @@ export class SSLConfig {
 }
 
 export class AppConfig {
+    allowedOrigins: string[];
+    cookieSigningSecret: string;
+    encryptionKey: string;
     host: string;
     port: number;
     ssl: SSLConfig | null;
-    cookieSigningSecret: string;
-    allowedOrigins: string[];
 }
 
 function hasSSLCertificateAndKey() {
@@ -29,6 +31,12 @@ function hasSSLCertificateAndKey() {
 
 export const appConfig = () =>
     ({
+        allowedOrigins: (
+            process.env[ENV_NAME_ALLOWED_ORIGINS] ||
+            'https://localhost.auth.dndmapp.net,https://localhost.api.dndmapp.net,https://localhost.dndmapp.net'
+        ).split(','),
+        cookieSigningSecret: process.env[ENV_NAME_COOKIE_SIGNING_SECRET],
+        encryptionKey: process.env[ENV_NAME_ENCRYPTION_KEY],
         host: process.env[ENV_NAME_HOST],
         port: parseNumber(process.env[ENV_NAME_PORT]),
         ssl: !hasSSLCertificateAndKey()
@@ -37,14 +45,24 @@ export const appConfig = () =>
                   cert: process.env[ENV_NAME_SSL_CERT_PATH],
                   key: process.env[ENV_NAME_SSL_KEY_PATH],
               },
-        cookieSigningSecret: process.env[ENV_NAME_COOKIE_SIGNING_SECRET],
-        allowedOrigins: (
-            process.env[ENV_NAME_ALLOWED_ORIGINS] ||
-            'https://localhost.auth.dndmapp.net,https://localhost.api.dndmapp.net,https://localhost.dndmapp.net'
-        ).split(','),
     }) satisfies AppConfig;
 
 export class EnvironmentVariables {
+    @IsString({ each: true })
+    @IsArray()
+    @Expose()
+    @IsOptional()
+    [ENV_NAME_ALLOWED_ORIGINS]: string[];
+
+    @IsString()
+    @MinLength(64)
+    @Expose()
+    [ENV_NAME_COOKIE_SIGNING_SECRET]: string;
+
+    @IsString()
+    @Expose()
+    [ENV_NAME_ENCRYPTION_KEY]: string;
+
     @IsString()
     @Expose()
     @IsOptional()
@@ -65,17 +83,6 @@ export class EnvironmentVariables {
     @Expose()
     @IsOptional()
     [ENV_NAME_SSL_KEY_PATH]: string;
-
-    @IsString()
-    @MinLength(64)
-    @Expose()
-    [ENV_NAME_COOKIE_SIGNING_SECRET]: string;
-
-    @IsString({ each: true })
-    @IsArray()
-    @Expose()
-    @IsOptional()
-    [ENV_NAME_ALLOWED_ORIGINS]: string[];
 }
 
 async function validateEnvironmentVariables(config: Record<string, unknown>) {
