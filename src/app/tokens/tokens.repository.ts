de66e3f4fee5485511ae7ab89f5e320afb3023fb
NoNamer777 <oscar.wellner@gmail.com>
2025-05-16 18:05:05 +0200

    Properly transform token metadata objects from the database

--- src/app/tokens/tokens.repository.ts
+++ src/app/tokens/tokens.repository.ts
@@ -3,6 +3,47 @@ import { plainToInstance } from 'class-transformer';
 import { DatabaseService } from '../database';
 import { TokenMetadata, transformUserRoles } from '../shared';
 
+const selectedTokenAttributes = {
+    select: {
+        jti: true,
+        pti: true,
+        sub: true,
+        tpe: true,
+        iss: true,
+        aud: true,
+        iat: true,
+        nbf: true,
+        exp: true,
+        rvk: true,
+        user: {
+            select: {
+                id: true,
+                username: true,
+                roles: {
+                    select: {
+                        role: {
+                            select: {
+                                id: true,
+                                name: true,
+                                scopes: {
+                                    select: {
+                                        scope: {
+                                            select: {
+                                                id: true,
+                                                name: true,
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
+} as const;
+
 @Injectable()
 export class TokensRepository {
     constructor(private readonly databaseService: DatabaseService) {}
@@ -10,52 +51,73 @@ export class TokensRepository {
     public findByJti = async (jti: string) =>
         plainToInstance(
             TokenMetadata,
-            await this.databaseService.token.findFirst({ where: { jti: jti }, include: { user: true } })
+            this.transformTokenUser(
+                await this.databaseService.token.findFirst({
+                    ...selectedTokenAttributes,
+                    where: { jti: jti },
+                })
+            )
         );
 
     public findAllByPti = async (pti: string) =>
-        plainToInstance(TokenMetadata, await this.databaseService.token.findMany({ where: { pti: pti } }));
+        plainToInstance(
+            TokenMetadata,
+            this.transformAllTokenUsers(
+                await this.databaseService.token.findMany({ ...selectedTokenAttributes, where: { pti: pti } })
+            )
+        );
 
     public findAllByUserIdAndNotRevoked = async (userId: string) =>
         plainToInstance(
             TokenMetadata,
-            await this.databaseService.token.findMany({ where: { AND: [{ sub: userId }, { rvk: false }] } })
+            this.transformAllTokenUsers(
+                await this.databaseService.token.findMany({
+                    ...selectedTokenAttributes,
+                    where: { AND: [{ sub: userId }, { rvk: false }] },
+                })
+            )
         );
 
     public create = async (metadata: TokenMetadata) =>
         plainToInstance(
             TokenMetadata,
-            await this.databaseService.token.create({
-                data: {
-                    tpe: metadata.tpe,
-                    rvk: metadata.rvk,
-                    sub: metadata.sub,
-                    iss: metadata.iss,
-                    aud: metadata.aud,
-                    iat: metadata.iat,
-                    exp: metadata.exp,
-                    nbf: metadata.nbf,
-                    pti: metadata.pti ?? null,
-                },
-            })
+            this.transformTokenUser(
+                await this.databaseService.token.create({
+                    ...selectedTokenAttributes,
+                    data: {
+                        tpe: metadata.tpe,
+                        rvk: metadata.rvk,
+                        sub: metadata.sub,
+                        iss: metadata.iss,
+                        aud: metadata.aud,
+                        iat: metadata.iat,
+                        exp: metadata.exp,
+                        nbf: metadata.nbf,
+                        pti: metadata.pti ?? null,
+                    },
+                })
+            )
         );
 
     public update = async (token: TokenMetadata) =>
         plainToInstance(
             TokenMetadata,
-            await this.databaseService.token.update({
-                where: { jti: token.jti },
-                data: {
-                    tpe: token.tpe,
-                    rvk: token.rvk,
-                    sub: token.sub,
-                    iss: token.iss,
-                    aud: token.aud,
-                    iat: token.iat,
-                    exp: token.exp,
-                    nbf: token.nbf,
-                },
-            })
+            this.transformTokenUser(
+                await this.databaseService.token.update({
+                    ...selectedTokenAttributes,
+                    where: { jti: token.jti },
+                    data: {
+                        tpe: token.tpe,
+                        rvk: token.rvk,
+                        sub: token.sub,
+                        iss: token.iss,
+                        aud: token.aud,
+                        iat: token.iat,
+                        exp: token.exp,
+                        nbf: token.nbf,
+                    },
+                })
+            )
         );
 
     public async removeByJti(jti: string) {
@@ -76,5 +138,16 @@ export class TokensRepository {
 
     public async removeAllExpired() {
         await this.databaseService.token.deleteMany({ where: { exp: { lt: new Date() } } });
+    }
+
+    private transformAllTokenUsers<T = unknown>(data: T[]) {
+        return data.map((token) => this.transformTokenUser(token));
+    }
+
+    private transformTokenUser<T = unknown>(data: T) {
+        if (data === null || typeof data !== 'object' || !('user' in data)) return data;
+        data.user = transformUserRoles(data.user);
+
+        return data;
     }
 }
