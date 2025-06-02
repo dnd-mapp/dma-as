bde42bf3e46d696ed49e2ac9ccf32f374e5ed651
NoNamer777 <oscar.wellner@gmail.com>
2025-06-02 23:45:26 +0200

    Fix ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,50 +1,33 @@
 import nx from '@nx/eslint-plugin';
-import pluginBan from 'eslint-plugin-ban';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
+    ...nx.configs['flat/base'],
+    ...nx.configs['flat/typescript'],
+    ...nx.configs['flat/javascript'],
     {
         ignores: ['**/dist'],
     },
     {
-        files: ['**/*.ts', '**/*.js'],
-        plugins: {
-            ban: pluginBan,
-        },
-        extends: [...nx.configs['flat/base'], ...nx.configs['flat/typescript'], ...nx.configs['flat/javascript']],
-        languageOptions: {
-            globals: {
-                ...globals.node,
-                ...globals.jest,
-            },
-            sourceType: 'commonjs',
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
+        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
         rules: {
             '@nx/enforce-module-boundaries': [
                 'error',
                 {
                     enforceBuildableLibDependency: true,
-                    allow: ['^.*/eslint\\.config\\.mjs$'],
+                    allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
                     depConstraints: [
                         {
                             sourceTag: '*',
-                            onlyDependOnLibsWithTags: ['*']
-                        }
-                    ]
-                }
-            ],
-            '@typescript-eslint/no-floating-promises': 'off',
-            '@typescript-eslint/no-unused-vars': 'off',
-            'ban/ban': [
-                2,
-                { name: 'fdescribe', message: 'Focussing test suites is not allowed' },
-                { name: 'fit', message: 'Focussing a single unit test is not allowed' },
+                            onlyDependOnLibsWithTags: ['*'],
+                        },
+                    ],
+                },
             ],
         },
-    }
-);
+    },
+    {
+        files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts', '**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
+        // Override or add rules here
+        rules: {},
+    },
+];
