fccf5efc4f31d80c3d53878283c5bf10c0109589
NoNamer777 <oscar.wellner@gmail.com>
2025-06-02 17:58:06 +0200

    Simplify Webpack config

--- apps/auth/webpack.config.js
+++ apps/auth/webpack.config.js
@@ -1,44 +1,16 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const GeneratePackageJsonPlugin = require('generate-package-json-webpack-plugin');
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { resolve } = require('path');
-const nodeExternals = require('webpack-node-externals');
-const packageJson = require('../../package.json');
-
-console.log(__dirname);
 
 const outputPath = resolve(__dirname, '../../dist/apps/auth');
 
-/** @type {import('webpack').Configuration} */
 const webpackConfig = {
-    entry: {
-        auth: resolve(__dirname, 'src/main.ts'),
-    },
-    externalsPresets: {
-        node: true,
-    },
-    externals: [nodeExternals()],
-    mode: process.env['NODE_ENV'] === 'production' ? 'production' : 'development',
-    node: {
-        __dirname: false,
-        __filename: false,
-    },
-    optimization: {
-        nodeEnv: false,
-    },
     output: {
-        clean: true,
         path: outputPath,
-        filename: 'main.js',
     },
     plugins: [
-        new GeneratePackageJsonPlugin(packageJson),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: resolve(__dirname, 'src/assets'),
-                    to: resolve(outputPath, 'assets'),
-                },
-            ],
+        new NxAppWebpackPlugin({
+            main: './src/main.ts',
+            tsConfig: './tsconfig.app.json',
         }),
     ],
 };
