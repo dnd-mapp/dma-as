16edd312f3b2b9f2033a027c9d7bba2c5c7818ef
NoNamer777 <oscar.wellner@gmail.com>
2025-06-13 23:58:35 +0200

    Fix Jest configurations

--- jest.config.ts
+++ jest.config.ts
@@ -3,31 +3,5 @@ import type { Config } from 'jest';
 
 export default async () =>
     ({
-        clearMocks: true,
-        collectCoverage: true,
-        collectCoverageFrom: [
-            '<rootDir>/src/**/*.ts',
-            '!<rootDir>/**/index.ts',
-            '!<rootDir>/**/main.ts',
-            '!<rootDir>/**/public_api.ts',
-            '!<rootDir>/**/*.d.ts',
-        ],
-        coverageReporters: ['text-summary', 'html'],
-        // Don't enforce code coverage for now to speed up development.
-        // coverageThreshold: {
-        //     global: {
-        //         branches: 80,
-        //         functions: 80,
-        //         lines: 80,
-        //         statements: 80,
-        //     },
-        // },
-        moduleFileExtensions: ['js', 'ts'],
         projects: await getJestProjectsAsync(),
-        randomize: true,
-        showSeed: true,
-        testEnvironment: 'node',
-        transform: {
-            '^.+\\.(t|j)s$': 'ts-jest',
-        },
     }) satisfies Config;
